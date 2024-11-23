d4574811e824843423541d133c19f00ad173b4ae
Hyun <itsmine096@gmail.com>
2024-11-23 16:55:48 +0900

    project init

---
+++ src/main/java/nambang_swag/bada_on/BadaOnServerApplication.java
@@ -0,0 +1,13 @@
+package nambang_swag.bada_on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadaOnServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadaOnServerApplication.class, args);
+	}
+
+}
