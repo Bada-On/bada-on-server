e71165e6840d16ceb331194c45f7eb7510dbff7d
Hyun <itsmine096@gmail.com>
2024-12-01 23:10:11 +0900

    [feat] Swagger 적용

--- src/main/java/nambang_swag/bada_on/controller/PlaceController.java
+++ src/main/java/nambang_swag/bada_on/controller/PlaceController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import nambang_swag.bada_on.request.PlaceRegister;
@@ -22,22 +23,19 @@ public class PlaceController {
 
 	private final PlaceService placeService;
 
+	@Operation(summary = "장소 등록", description = "장소를 등록합니다.")
 	@PostMapping
 	public ResponseEntity<PlaceInfo> register(@RequestBody @Valid PlaceRegister placeRegister) {
 		return ResponseEntity.ok().body(placeService.register(placeRegister));
 	}
 
+	@Operation(summary = "모든 등록 장소 조회", description = "모든 등록 장소와 장소에서 가능한 활동들을 조회합니다.")
 	@GetMapping
 	public ResponseEntity<PlaceList> getList() {
 		return ResponseEntity.ok().body(placeService.getListOfPlaces());
 	}
 
-	@GetMapping("/loc")
-	public ResponseEntity<PlaceList> getNearestPlace(@RequestParam(name = "lat") double lat,
-		@RequestParam(name = "lon") double lon) {
-		return ResponseEntity.ok().body(placeService.findNearestPlace(lon, lat));
-	}
-
+	@Operation(summary = "활동을 통한 등록 장소 조회", description = "활동으로 필터링된 등록 장소들을 조회합니다.")
 	@GetMapping("/find")
 	public ResponseEntity<PlaceList> getPlaceByActivity(@RequestParam String activity) {
 		return ResponseEntity.ok().body(placeService.findPlaceByActivity(activity));
--- src/main/java/nambang_swag/bada_on/controller/WeatherController.java
+++ src/main/java/nambang_swag/bada_on/controller/WeatherController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import nambang_swag.bada_on.response.WeatherDetail;
 import nambang_swag.bada_on.response.WeatherSummary;
-import nambang_swag.bada_on.service.ExternalApiService;
 import nambang_swag.bada_on.service.WeatherService;
 
 @RequiredArgsConstructor
@@ -21,8 +21,8 @@ import nambang_swag.bada_on.service.WeatherService;
 public class WeatherController {
 
 	private final WeatherService weatherService;
-	private final ExternalApiService externalApiService;
-
+	
+	@Operation(summary = "등록장소에서의 활동 추천도 조회", description = "입력된 위치의 활동 추천도 리스트를 조회")
 	@GetMapping("/summary/{id}")
 	public ResponseEntity<List<WeatherSummary>> getWeatherSummary(
 		@PathVariable("id") Long id,
@@ -30,6 +30,7 @@ public class WeatherController {
 		return ResponseEntity.ok().body(weatherService.getWeatherSummary(id, category));
 	}
 
+	@Operation(summary = "등록장소의 세부 날씨 조회", description = "등록 장소의 세부 날씨 조회")
 	@GetMapping("/details/{id}")
 	public ResponseEntity<WeatherDetail> getWeatherDetails(@PathVariable("id") Long id) {
 		return ResponseEntity.ok().body(weatherService.getWeatherDetail(id));
