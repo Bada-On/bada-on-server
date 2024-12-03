701803481e3506366b4ba975b6ac5639bd796fe5
Hyun <itsmine096@gmail.com>
2024-12-03 23:38:23 +0900

    [doc] Swagger API가 호스팅 되는 서버 목록을 정의

--- src/main/java/nambang_swag/bada_on/config/SwaggerConfig.java
+++ src/main/java/nambang_swag/bada_on/config/SwaggerConfig.java
@@ -3,9 +3,17 @@ package nambang_swag.bada_on.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+@OpenAPIDefinition(
+	servers = {
+		@Server(url = "https://badaon.shop", description = "개발 서버"),
+		@Server(url = "http://localhost:8080", description = "로컬 서버")
+	}
+)
 @Configuration
 public class SwaggerConfig {
 
