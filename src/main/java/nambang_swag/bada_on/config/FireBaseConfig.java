94b0e24b0e7e11a06e4d386357e15c65250dc402
Hyun <itsmine096@gmail.com>
2025-01-08 13:50:14 +0900

    [build] firebase 환경변수 주입 방식 변경

--- src/main/java/nambang_swag/bada_on/config/FireBaseConfig.java
+++ src/main/java/nambang_swag/bada_on/config/FireBaseConfig.java
@@ -19,25 +19,10 @@ public class FireBaseConfig {
 
 	@Bean
 	public FirebaseApp initializeFirebase() throws IOException {
-		String firebaseKey = System.getenv("FIREBASE_KEY");
-		File keyFile = new File("/app/config/firebase-key.json");
-
-		if (firebaseKey != null && !firebaseKey.isEmpty()) {
-			// 환경 변수에서 키를 읽는 경우
-			InputStream serviceAccount = new ByteArrayInputStream(firebaseKey.getBytes(StandardCharsets.UTF_8));
-			FirebaseOptions options = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.build();
-			return FirebaseApp.initializeApp(options);
-		} else if (keyFile.exists()) {
-			// 파일에서 키를 읽는 경우
-			FileInputStream serviceAccount = new FileInputStream(keyFile);
-			FirebaseOptions options = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.build();
-			return FirebaseApp.initializeApp(options);
-		} else {
-			throw new IllegalStateException("Firebase 인증 정보를 찾을 수 없습니다.");
-		}
+		FileInputStream serviceAccount = new FileInputStream("/app/firebase-service-account.json");
+		FirebaseOptions options = FirebaseOptions.builder()
+			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			.build();
+		return FirebaseApp.initializeApp(options);
 	}
 }
